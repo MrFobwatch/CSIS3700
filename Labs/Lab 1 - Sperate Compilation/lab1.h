2090abdeb3453491c1ea12bb06d47288079a1081
Kevin Yacucci <kcy0511@gmail.com>
2017-09-01 11:47:50 -0400

    Finished Lab 1

---
+++ Labs/Lab 1 - Sperate Compilation/lab1.h
@@ -0,0 +1,11 @@
+#include <iostream> //List of things that go in header file
+			//Includes
+			//Costants, Defines, Macros
+			//Data Structures (double points off)
+			//Global Variables
+			//Function Prototypes
+using namespace std;
+int readData(int[],int);
+void sortData(int[],int);
+void printData(int[],int);
+
