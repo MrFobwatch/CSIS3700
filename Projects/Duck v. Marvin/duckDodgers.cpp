36fac4dd9e9b922f2e6543ad0cab272e75069748
MrFobwatch <kcy0511@gmail.com>
2017-11-13 20:54:07 -0500

    Create getInput
    Also add some documentation

--- Projects/Duck v. Marvin/duckDodgers.cpp
+++ Projects/Duck v. Marvin/duckDodgers.cpp
@@ -3,3 +3,39 @@
 //
 
 #include "duckDodgers.h"
+
+void duckDodgers::getInput() {
+    std::cin >> startDuck.row >> startDuck.column;
+    std::cin >> startMarvin.row >> startMarvin.column;
+    std::cin >> startPhos.row >> startPhos.column;
+    std::cin >> numLava;
+    for (int i = 0; i < numLava; ++i) {
+        int row, column;
+        std::cin >> row >> column;
+        lavaList.insert(i, Coordinate(row,column));
+    }
+}
+
+void duckDodgers::genMap() {
+
+}
+
+void duckDodgers::genPaths() {
+
+}
+
+void duckDodgers::lookAtNeighbors() {
+
+}
+
+void duckDodgers::assignValue() {
+
+}
+
+void duckDodgers::placeGandalf() {
+
+}
+
+void duckDodgers::outputResults() {
+
+}
--- Projects/Duck v. Marvin/duckDodgers.h
+++ Projects/Duck v. Marvin/duckDodgers.h
@@ -5,23 +5,40 @@
 #ifndef DUCK_V_MARVIN_DUCKDODGERS_H
 #define DUCK_V_MARVIN_DUCKDODGERS_H
 
+#include <iostream>
 #include "queue.h"
 #include "list.h"
 
 class duckDodgers {
 public:
-    void getInput();
-    void genMap();
-    void genPaths();
-    void lookAtNeighbors();
-    void assignValue();
-    void placeGandalf();
-    void outputResults();
+    void getInput(); //gets values of Marvin and Duck location and creates Lava Cell info
+    void genMap(); //sets locations of lava and assigns known values
+    void genPaths(); //runs BreadthFirstSearch
+    void lookAtNeighbors();  //adds cells to a queue for checking
+    void assignValue(); //parses a queue and sets values. Once empty exit function
+    void placeGandalf(); //takes marvin path and runs genPath for each cell in the path
+    void outputResults(); //Returns all necessary info
 
 private:
     int map[20][20];
+    Coordinate startDuck,
+               startMarvin,
+               startPhos;
 
+    int numLava;
+    LinearList<Coordinate> lavaList;
 };
 
+struct Coordinate {
+    int row, column;
+    Coordinate(int row, int column) : row(row), column(column) {}
+    bool operator==(const Coordinate& other) const {
+        return other.row == row && other.column == column;
+    }
+
+    bool operator!=(const Coordinate& other) const {
+        return !(*this == other);
+    }
+};
 
 #endif //DUCK_V_MARVIN_DUCKDODGERS_H
