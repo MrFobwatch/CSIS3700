624431bb65d81d3bf09f2af90ed35d268d318cad
MrFobwatch <kcy0511@gmail.com>
2017-12-08 10:46:06 -0500

    Add FamilyTree Class

---
+++ Projects/FamilyTree/familyTree.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Kevin on 12/8/2017.
+//
+
+#include "familyTree.h"
---
+++ Projects/FamilyTree/familyTree.h
@@ -0,0 +1,16 @@
+//
+// Created by Kevin on 12/8/2017.
+//
+
+#ifndef FAMILYTREE_FAMILYTREE_H
+#define FAMILYTREE_FAMILYTREE_H
+
+#include <iostream>
+#include
+
+class familyTree {
+
+};
+
+
+#endif //FAMILYTREE_FAMILYTREE_H
